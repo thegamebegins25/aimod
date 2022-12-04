4eb1046b467e47605a332293ffab87b0ec94a75d
thegamebegins25 <wdppostcard@gmail.com>
2022-12-03 19:30:23 -0500

    totally source code

---
+++ main.js
@@ -0,0 +1,18 @@
+const video = document.getElementById("video");
+const code = document.getElementById("code");
+const button = document.getElementById("button");
+
+function rick() {
+    console.log("click");
+    video.style = "display: inline-block";
+    code.style = "display: none";
+    button.style = "display: none";
+    video.play();
+    if (video.requestFullscreen) {
+        video.requestFullscreen();
+      } else if (video.webkitRequestFullscreen) { /* Safari */
+        video.webkitRequestFullscreen();
+      } else if (video.msRequestFullscreen) { /* IE11 */
+        video.msRequestFullscreen();
+      }
+}
